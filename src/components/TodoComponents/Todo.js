d7c9d8b228de2dfc29e5dddf5a98a7e23e5cedf3
brellin <cekakid@gmail.com>
2019-03-20 14:08:13 -0500

    style started

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Todo.css';
+
+const Todo = props => {
+    return (
+        <div className='todos'>
+            <h1>Todo List:</h1>
+        </div>
+    );
+}
+
+export default Todo;
